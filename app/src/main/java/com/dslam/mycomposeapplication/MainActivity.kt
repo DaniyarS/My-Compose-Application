725a7a4fbb6ddb329106d3d86eff0e656aefaa4f
Daniyar Slamkul <d.slamkul@aviata.me>
2024-08-18 18:03:56 +0500

    multiplication table

--- app/src/main/java/com/dslam/mycomposeapplication/MainActivity.kt
+++ app/src/main/java/com/dslam/mycomposeapplication/MainActivity.kt
@@ -3,15 +3,26 @@ package com.dslam.mycomposeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dslam.mycomposeapplication.ui.theme.MyComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,24 +31,39 @@ class MainActivity : ComponentActivity() {
         setContent(
             content = {
                 MyComposeApplicationTheme {
-                    UserInfo(name = "Daniyar", age = 24)
+                    TimesTable()
                 }
             }
         )
     }
 }
 
-@Composable
-private fun UserInfo(name: String, age: Int) {
-    Column {
-        repeat(10) {
-            Text(text = "Hello $name! You are $age years old!")
-        }
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingUser() {
-    UserInfo(name = "Daniyar", age = 12)
+private fun TimesTable() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        repeat(9) { column ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                repeat(9) { row ->
+                    Box(
+                        modifier = Modifier
+                            .background(color = if ((row + column) % 2 == 0) Color.Yellow else Color.White)
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .border(width = 1.dp, color = Color.DarkGray),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "${(row + 1) * (column + 1)}")
+                    }
+                }
+            }
+        }
+    }
 }
