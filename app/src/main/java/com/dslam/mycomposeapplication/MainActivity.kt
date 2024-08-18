85602ea8d4bcc1e557a41e5a06c22fa906e0972d
Daniyar Slamkul <d.slamkul@aviata.me>
2024-08-18 17:37:17 +0500

    intro to compose; column component

--- app/src/main/java/com/dslam/mycomposeapplication/MainActivity.kt
+++ app/src/main/java/com/dslam/mycomposeapplication/MainActivity.kt
@@ -3,41 +3,41 @@ package com.dslam.mycomposeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.dslam.mycomposeapplication.ui.theme.MyComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyComposeApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+        setContent(
+            content = {
+                MyComposeApplicationTheme {
+                    UserInfo(name = "Daniyar", age = 24)
                 }
             }
+        )
+    }
+}
+
+@Composable
+private fun UserInfo(name: String, age: Int) {
+    Column {
+        repeat(10) {
+            Text(text = "Hello $name! You are $age years old!")
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyComposeApplicationTheme {
-        Greeting("Android")
-    }
+fun GreetingUser() {
+    UserInfo(name = "Daniyar", age = 12)
 }
